208969688501819fb4b3082df050880700220b2e
olivefi <fischer.oliver@hotmail.com>
2022-12-08 10:17:18 -0800

    3.2

--- src/03/main.rs
+++ src/03/main.rs
@@ -4,10 +4,9 @@ fn main(){
     let data = read_in_file("data.txt".to_string());
     let mut in_common: Vec<char> = Vec::new();
     let mut data_merged: Vec<String> = Vec::new();
+
     for pair in data {
         data_merged.push(pair[0].to_string() + &pair[1].to_string());
-    }
-    for pair in data {
         let common: char = char_in_common(pair[0].to_string(), pair[1].to_string());
         in_common.push(common);
     }
@@ -17,7 +16,15 @@ fn main(){
     }
     println!("Score: {}", score);
 
-    
+    let mut score_groups: u32 = 0;
+    let groups: u32 = (data_merged.len()/3) as u32;
+    for i in 0..groups {
+        let mut common: String = chars_in_common(data_merged[(i*3) as usize].to_string(), data_merged[(i*3+1) as usize].to_string());
+        let common_c: char = char_in_common(common, data_merged[(i*3+2) as usize].to_string());
+        score_groups += common_c.to_digit(36).unwrap() - 9 + 26*(common_c.is_uppercase() as u32);
+    }
+    println!("Score Groups: {}", score_groups);
+
 }
 
 fn read_in_file(name: String) -> Vec<Vec<String>> {
@@ -48,10 +55,10 @@ fn char_in_common(a: String, b: String) -> char {
 }
 
 fn chars_in_common(a: String, b: String) -> String {
-    let mut common: String;
+    let mut common: String = "".to_string();
     for c in a.chars() {
         if b.contains(c) {
-            common += c;
+            common += &c.to_string();
         }
     }
     common
